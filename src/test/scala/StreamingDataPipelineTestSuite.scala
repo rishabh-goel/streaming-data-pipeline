e92935c8d003fd6a3878aa3b8b68993e56f235a8
rishabhgoel <goel.rishabh12@gmaill.com>
2021-12-03 20:26:29 -0600

    [RG] : Created testScript.sh, sampleLogfile.txt and added testcases

--- src/test/scala/StreamingDataPipelineTestSuite.scala
+++ src/test/scala/StreamingDataPipelineTestSuite.scala
@@ -1,6 +1,8 @@
 
 import org.scalatest.funspec.AnyFunSpec
 import com.typesafe.config.ConfigFactory
+import scala.sys.process._
+import scala.io.Source
 
 
 
@@ -19,6 +21,33 @@ class StreamingDataPipelineTestSuite extends AnyFunSpec {
     }
     it("should have a topic name") {
       assert(config.getString("spark.topic-name") == "topic1")
+    }
+  }
+
+  describe("Map Reduce"){
+    it("should count number of ERROR messages") {
+      val conf = ConfigFactory.load()
+      val filename = conf.getString("sampleLogFileLocation")
+      val buf = scala.collection.mutable.ListBuffer.empty[Int]
+      val lines = Source.fromFile(filename).getLines.toList
+      lines.foreach(line => {
+        val logEntry = line.split(" - ").map(_.trim)
+        val logParser = logEntry(0).replace("  ", " ").split(" ").map(_.trim)
+        val logMsgType = logParser(2)
+        if (logMsgType=="ERROR") {
+          buf += 1
+        }
+      })
+
+      assert(buf.size > 1)
+    }
+  }
+
+  describe("Shell command"){
+    it("should execute script") {
+      val result = Process("sh Shell-Scripts/testScript.sh").!!
+
+      result === "Hello"
     }
   }
 }
